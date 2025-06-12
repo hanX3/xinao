a0ff7a0a41d8c80d247294ea1e4f220ad01e9d2b
hanX3 <hanxingchi@gmail.com>
2025-06-12 17:45:02 +0800

    test tdms file

---
+++ raw2root/test_tdms.py
@@ -0,0 +1,25 @@
+from nptdms import TdmsFile
+
+tdms_file = TdmsFile.read("/mnt/e/11Bpg/data/Date_2025526101424.tdms")
+
+print("File Properties:")
+for key, value in tdms_file.properties.items():
+    print(f"  {key}: {value}")
+
+print("Groups:")
+for group in tdms_file.groups():
+    print(" -", group.name)
+print(f"Group '{group.name}' Properties:")
+for key, value in group.properties.items():
+    print(f"  {key}: {value}")
+
+for group in tdms_file.groups():
+    print(f"\nGroup: {group.name}")
+    for channel in group.channels():
+        length = len(channel)
+        print(f"Group: {group}, Channel: {channel.name}, Length: {length}")
+
+        print(f"Channel '{channel.name}' Properties:")
+        for key, value in channel.properties.items():
+            print(f"  {key}: {value}")
+
