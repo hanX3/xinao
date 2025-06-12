4774af61fd25d1cf97cce42a12e820076c2e7e99
hanX3 <hanxingchi@gmail.com>
2025-06-12 17:44:16 +0800

    for axuv

--- raw2root/tdms2tree.py
+++ raw2root/tdms2tree.py
@@ -8,16 +8,15 @@ from array import array
 import argparse
 
 #
-DATA_DIR = "/mnt/c/Users/hanX/OneDrive/works/11Bpg/dpp/data/"
+DATA_DIR = "/mnt/e/11Bpg/data/"
 GP = "DAQ Data"
 CH = "CH01"
 
 #
 def read_tdms_file(filename):
-    time_str = filename.split('/')[-2].split('_')[1]
     try:
         tdms_file = TdmsFile.read(filename)
-        return tdms_file, time_str
+        return tdms_file
     except Exception as e:
         print(f"can not open tdms file {e}")
         return None
@@ -42,7 +41,7 @@ def extract_channel_data(group):
     return data_dict, props_dict
 
 #
-def tdms2tree(time_str, data, props):
+def tdms2tree_sic(time_str, data, props):
     fo = ROOT.TFile(f"../rootfile/{time_str}.root", "RECREATE")
     length = props['Length']
     wf_increment = props['wf_increment']
@@ -70,20 +69,45 @@ def tdms2tree(time_str, data, props):
     fo.Close()
     print(f"Tree saved to {time_str}.root with {n_entry} entries.")
 
+#
+def tdms2tree_axuv(time_str, data, props):
+    fo = ROOT.TFile(f"../rootfile/{time_str}.root", "RECREATE")
+    length = props['wf_samples']
+
+    print(f"length {length}")
+
+    tr = ROOT.TTree(f"tr", f"{length}")
+
+    w = array('h', [0]*length)
+    tr.Branch("w", w, f"w[{length}]/S")
+
+    n_entry = len(data) // length
+    for i in range(n_entry):
+        entry = data[i * length : (i + 1) * length]
+        for j in range(length):
+            w[j] = entry[j]
+        tr.Fill()
+
+    tr.Write()
+    fo.Close()
+    print(f"Tree saved to {time_str}.root with {n_entry} entries.")
+
+
 ##
 def main(data_name):
     print(f"Processing: {data_name}")
 
-    tdms_file_name = f"{DATA_DIR}{data_name}/{data_name}.tdms"
-    tdms_file, time_str = read_tdms_file(tdms_file_name)
+    tdms_file_name = f"{DATA_DIR}Date_{data_name}.tdms"
+    print(f"{tdms_file_name}")
+    tdms_file  = read_tdms_file(tdms_file_name)
     if tdms_file is None:
         return
 
     data_dict, props_dict = extract_channel_data(tdms_file[GP])
     data = data_dict[CH]
     props = props_dict[CH]
-    tdms2tree(time_str, data, props)
-  
+    tdms2tree_axuv(data_name, data, props)
+
 ##
 if __name__ == "__main__":
     par = argparse.ArgumentParser(description="Process data by name.")
